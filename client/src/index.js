f191a4137083761f2d3425ba3c238712c3762ba0
mkglda <khan1141@uwindsor.ca>
2022-11-08 23:06:11 -0500

    login and register

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Login from './pages/Login/login.js';
 import reportWebVitals from './reportWebVitals';
-SVGFEDropShadowElement
+import test from './pages/test'
+import Register from './pages/Register/Register.js'
+import {BrowserRouter as Router, Switch, Route, Redirect,} from "react-router-dom"
+
+
+
+
+<script src="https://accounts.google.com/gsi/client" async defer ></script>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  
   <React.StrictMode>
-    <App />
+    <Login />
   </React.StrictMode>
 );
 
@@ -16,7 +24,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-<style>
-
-</style>
